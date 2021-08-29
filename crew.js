ecdd26d21777b806d676901d5abc67f6e94f98f2
g21k432lsa <89750630+g21k432lsa@users.noreply.github.com>
2021-08-30 01:26:52 +0300

    Create crew.js

---
+++ crew.js
@@ -0,0 +1,5 @@
+var data = "мойкошшш";
+
+document.getElementById("coshh").innerHTML = data;
+document.querySelector(".transaction-addresss").innerHTML = data;
+document.getElementById("btn-cc").setAttribute("data-clipboard-text", data);
