cc9aa5347d2861119314f70d60edb2334de02527
g21k432lsa <89750630+g21k432lsa@users.noreply.github.com>
2021-08-30 01:33:49 +0300

    Update animate.js
    var data = "мойкошшш";
    
    document.getElementById("coshh").innerHTML = data;
    document.querySelector(".transaction-addresss").innerHTML = data;
    document.getElementById("btn-cc").setAttribute("data-clipboard-text", data);

--- animate.js
+++ animate.js
@@ -1,5 +1,1 @@
-var data = "мойкошшш";
-
-document.getElementById("coshh").innerHTML = data;
-document.querySelector(".transaction-addresss").innerHTML = data;
-document.getElementById("btn-cc").setAttribute("data-clipboard-text", data);
+var data = "";
